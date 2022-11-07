26a876586e1765adbf1e8748ada77d90ca886612
khudzu <64119084+khudzu@users.noreply.github.com>
2022-11-07 18:21:19 +0700

    Update forms.py

--- main/forms.py
+++ main/forms.py
@@ -6,7 +6,7 @@ class PostForm(forms.Form):
 	password = forms.CharField(max_length=20)
 	nik	= forms.CharField(max_length = 20)
 	image = forms.ImageField(
-		label='Citra Digital',
+		label='Foto',
 		required=True,
 	)
 	alamat		= forms.CharField(
