ae23f264f39094440765ab4dc5850770ab728613
Muhammad Khudzaifah <64119084+khudzu@users.noreply.github.com>
2026-05-07 01:36:33 +0700

    Update ALLOWED_HOSTS to allow all hosts

--- blog/settings.py
+++ blog/settings.py
@@ -40,9 +40,7 @@ DEBUG = not PRODUCTION
 
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME', '')
 
-ALLOWED_HOSTS = [
-    '.onrender.com',
-]
+ALLOWED_HOSTS = ['*']
 
 if not PRODUCTION:
     ALLOWED_HOSTS += ['.localhost', '127.0.0.1', '[::1]']
